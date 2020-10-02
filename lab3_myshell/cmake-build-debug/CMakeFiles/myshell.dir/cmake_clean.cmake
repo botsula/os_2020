2cd8e6491b399188e27f31b7a97e0e5111084bd5
Anna Botsula <anna.botsula@gmail.com>
2020-10-02 14:21:58 +0300

    before comment

---
+++ lab3_myshell/cmake-build-debug/CMakeFiles/myshell.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myshell.dir/src/main.cpp.o"
+  "myshell"
+  "myshell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myshell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
